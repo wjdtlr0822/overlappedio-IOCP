ae75d41afe9e68e86557f20063083d91ca4ce630
wjdtlr0822 <a01055687052@gmail.com>
2022-12-29 23:09:12 +0900

    ㅎ

--- server_chat7/ClientInfo.h
+++ server_chat7/ClientInfo.h
@@ -1,1 +1,35 @@
 #pragma once
+#include "Define.h"
+
+#include <stdio.h>
+#include <mutex>
+#include <queue>
+
+class stClientInfo
+{
+public:
+
+private:
+
+	bool SendIO() {
+		auto sendOverlappedEx = mSendDataqueue.front();
+
+		DWORD dwRecvNumBytes = 0;
+
+	}
+
+	INT32 mIndex = 0;
+	HANDLE mIOCPHandle = INVALID_HANDLE_VALUE;
+	INT64 mIsConnect = 0;
+	UINT64 mLatestClosedTimeSec = 0;
+	SOCKET mSocket;
+
+	stOverlappedEx mAcceptContext;
+	char mAcceptBuf[64];
+
+	stOverlappedEx mRecvOverlappedEx;
+	char mRecvBuf[MAX_SOCK_RECVBUF];
+
+	std::mutex mSendLock;
+	std::queue<stOverlappedEx*> mSendDataqueue;
+};
--- server_chat7/Define.h
+++ server_chat7/Define.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+
+const UINT32 MAX_SOCK_RECVBUF = 256;
+const UINT32 MAX_SOCK_SENDBUF = 4096;
+const UINT32 RE_USE_SESSION_WAIT_TIMESEC = 3;
+
+
+enum class IOOPeration {
+	RECV,
+	SEND
+};
+
+struct stOverlappedEx {
+	WSAOVERLAPPED m_wsaOverlapped;
+	WSABUF m_wsaBuf;
+	IOOPeration m_eOperation;
+	UINT32 SessionIndex = 0;
+};
